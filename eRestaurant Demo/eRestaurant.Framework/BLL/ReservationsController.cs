065fb87373675da31776c2d10073a26fa1466c22
kholman4 <keiraholman4@gmail.com>
2015-10-22 16:38:30 -0600

    Data Object Method for Reservations

--- eRestaurant Demo/eRestaurant.Framework/BLL/ReservationsController.cs
+++ eRestaurant Demo/eRestaurant.Framework/BLL/ReservationsController.cs
@@ -7,6 +7,7 @@ using System.ComponentModel; //comes with entity framework
 using eRestaurant.Framework.DAL;
 using eRestaurant.Framework.Entities;
 using eRestaurant.Framework.Entities.POCOs;
+using eRestaurant.Framework.Entities.DTO;
 
 namespace eRestaurant.Framework.BLL
 {
@@ -26,6 +27,35 @@ namespace eRestaurant.Framework.BLL
                                  Description = eachRow.Description
                              };
                  return result.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<DailyReservation> ListUpcomingReservations(string eventCode)
+        {
+            using (var context = new RestaurantContext())
+            {
+                var result = from eachRow in context.Reservations
+                             where eachRow.ReservationStatus == "B"
+                             //TBA - && eachRow has the correct event code  (&& eachRow.EventCode == 'S')
+                             orderby eachRow.ReservationDate
+                             group eachRow by new { eachRow.ReservationDate.Month,     eachRow.ReservationDate.Day } //start the code from beginning up until this point will give you all the details of the reservations
+                             into dailyReservation
+                             select new DailyReservation()   
+                                 { //DailyReservation() //Create a DTO class called DailyReservation    
+                                     Month = dailyReservation.Key.Month,
+                                     Day = dailyReservation.Key.Day,
+                                     Reservations = from booking in dailyReservation
+                                                    select new Booking()
+                                                    { //Booking() //Create a Booking POCO class
+                                                        Name = booking.CustomerName,
+                                                        NumberInParty = booking.NumberInParty,
+                                                        Time =          booking.ReservationDate.TimeOfDay,
+                                                        Phone = booking.ContactPhone,
+                                                        Event = booking.SpecialEvent.Description
+                                                    }
+                                 };
+                return result.ToList();
             }
         }
     }
