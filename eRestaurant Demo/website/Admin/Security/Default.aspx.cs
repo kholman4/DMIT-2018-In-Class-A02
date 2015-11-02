a305f85f532c2b6c4b0ce620f11f234762488d44
kholman4 <keiraholman4@gmail.com>
2015-11-02 16:36:19 -0700

    Added data bind to role and user

--- eRestaurant Demo/website/Admin/Security/Default.aspx.cs
+++ eRestaurant Demo/website/Admin/Security/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eRestaurant.Framework.BLL.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,43 @@ public partial class Admin_Security_Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if(!IsPostBack)
+        {
+            DataBindUserList();
+            DataBindRoleList();
+        }
+    }
 
+    protected void RolesListView_ItemCommand(object sender, ListViewCommandEventArgs e)
+    {
+        if (e.CommandName == "AddDefaultRoles")
+        {
+            var controller = new RoleManager();
+            controller.AddDefaultRoles();
+            DataBindRoleList();
+        }
+    }
+
+    private void DataBindRoleList()
+    {
+        //populate the roles info
+        RolesListView.DataSource = new RoleManager().Roles.ToList();
+        RolesListView.DataBind();
+    }
+
+    private void DataBindUserList()
+    {
+        //Populate the users info
+        UserListView.DataSource = new UserManager().Users.ToList();
+        UserListView.DataBind();
+    }
+    protected void UserListView_ItemCommand(object sender, ListViewCommandEventArgs e)
+    {
+        if(e.CommandName == "AddDefaultUsers")
+        {
+            var controller = new UserManager();
+            controller.AddDefaultUsers();
+            DataBindUserList(); //to refresh the data in the list view
+        }
     }
 }
