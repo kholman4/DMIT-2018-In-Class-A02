43bf07792e10e82d810e2c3c5509910705f46afe
kholman4 <keiraholman4@gmail.com>
2015-11-09 15:53:47 -0700

    Clockpicker & created user controls

---
+++ eRestaurant Demo/website/App_Code/ProcessRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EatIn.UI
+{
+    //ProcessRequest is a new Delegate datatype - used for holding a reference to a method that can be called.
+
+    public delegate void ProcessRequest();
+}
